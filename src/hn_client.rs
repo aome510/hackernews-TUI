554a30627dd620de141ee9d8a9a8517e2f8ab40b
Thang Pham <phamducthang1234@gmail.com>
2021-02-20 17:55:25 +0900

    add comments, use anyhow for error handling, and modify HNClient structure

--- src/hn_client.rs
+++ src/hn_client.rs
@@ -1,55 +1,105 @@
-use serde::{Deserialize, Serialize};
 use futures::future::join_all;
+use serde::{de::DeserializeOwned, Deserialize};
+
+use anyhow::Result;
 
 const HN_URI_PREFIX: &str = "https://hacker-news.firebaseio.com/v0/";
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize)]
 /// Story represents a story post in Hacker News.
 pub struct Story {
     pub title: String,
     pub url: String,
-    pub time: i64
+    pub time: i64,
+    id: i32,
+    kids: Vec<i32>,
+}
+
+#[derive(Debug, Deserialize)]
+/// Comment represents a comment in Hacker News.
+pub struct Comment {
+    kids: Vec<i32>,
+    subcomments: Vec<Box<Comment>>,
+    pub text: String,
+    pub by: String,
+    pub time: i64,
 }
 
 /// HNClient is a http client to communicate with Hacker News APIs.
 pub struct HNClient {
-    client: reqwest::Client
+    client: reqwest::Client,
+}
+
+impl Story {
+    /// Retrieve all comments in a story post
+    pub async fn get_all_comments(&self, hn_client: &HNClient) -> Result<Vec<Comment>> {
+        hn_client.get_items_from_ids::<Comment>(&self.kids).await
+    }
+}
+
+impl Comment {
+    /// Retrieve all subcomments of a comment
+    pub async fn get_all_subcomments(&mut self, hn_client: &HNClient) -> Result<()> {
+        self.subcomments = hn_client
+            .get_items_from_ids::<Comment>(&self.kids)
+            .await?
+            .into_iter()
+            .map(|comment| Box::new(comment))
+            .collect();
+        Ok(())
+    }
 }
 
 impl HNClient {
     /// Create new Hacker News Client
     pub fn new() -> HNClient {
         HNClient {
-            client: reqwest::Client::new()
+            client: reqwest::Client::new(),
         }
     }
 
-    async fn get_story_from_id(&self, id: i32) -> Result<Story, Box<dyn std::error::Error>> {
+    /// Retrieve data from an item id and parse it to the corresponding struct
+    pub async fn get_item_from_id<T>(&self, id: i32) -> Result<T>
+    where
+        T: DeserializeOwned,
+    {
         let request_url = format!("{}/item/{}.json", HN_URI_PREFIX, id);
-        Ok(self.client.get(&request_url)
+        Ok(self
+            .client
+            .get(&request_url)
             .send()
             .await?
-            .json::<Story>()
+            .json::<T>()
             .await?)
     }
 
+    /// Retrieve data of multiple items from their ids and parse to a Vector of
+    /// the corresponding struct
+    pub async fn get_items_from_ids<T>(&self, ids: &Vec<i32>) -> Result<Vec<T>>
+    where
+        T: DeserializeOwned,
+    {
+        let results = join_all(ids.iter().map(|id| self.get_item_from_id::<T>(*id))).await;
+        Ok(results
+            .into_iter()
+            .filter(|result| match result {
+                Ok(_) => true,
+                Err(_) => false,
+            })
+            .map(|result| result.unwrap())
+            .collect::<Vec<T>>())
+    }
+
     /// Retrieve a list of HN's top stories
-    pub async fn get_top_stories(&self) -> Result<Vec<Story>, Box<dyn std::error::Error>> {
+    pub async fn get_top_stories(&self) -> Result<Vec<Story>> {
         let request_url = format!("{}/topstories.json", HN_URI_PREFIX);
-        let story_ids = self.client.get(&request_url)
+        let story_ids = self
+            .client
+            .get(&request_url)
             .send()
             .await?
             .json::<Vec<i32>>()
             .await?;
-        let results = join_all(story_ids.into_iter().map(|id| self.get_story_from_id(id))).await;
-        Ok(results.into_iter()
-            .filter(|result| {
-                match result {
-                    Ok(_) => true,
-                    Err(_) => false
-                }
-            })
-            .map(|result| result.unwrap())
-            .collect::<Vec<Story>>())
+        self.get_items_from_ids::<Story>(&story_ids).await
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use cursive::{
     views::{TextView, ListView}
 };
 
+// modules
 mod hn_client;
 
 use hn_client::HNClient;
@@ -10,27 +11,32 @@ use hn_client::HNClient;
 #[tokio::main]
 async fn main() {
     let client = HNClient::new();
-    if let Ok(stories) = client.get_top_stories().await {
-        let stories = stories.into_iter()
-            .map(|story| format!("title: {}, url: {}", story.title, story.url))
-            .collect::<Vec<String>>();
+    match client.get_top_stories().await {
+        Ok(stories) => {
+            let stories = stories.into_iter()
+                .map(|story| format!("title: {}, url: {}", story.title, story.url))
+                .collect::<Vec<String>>();
 
-        let mut siv = cursive::default();
+            let mut siv = cursive::default();
 
-        // load theme
-        siv.load_toml(include_str!("../theme.toml")).unwrap();
+            // load theme
+            siv.load_toml(include_str!("../theme.toml")).unwrap();
 
-        siv.add_layer(ListView::new()
-                      .with(|list| {
-                          stories.iter().enumerate().for_each(|(id, story)| {
-                              list.add_child(
-                                  &format!("{}.", id),
-                                  TextView::new(story)
-                              );
-                          });
-                      })
-                      .scrollable());
-        siv.add_global_callback('q', |s| s.quit());
-        siv.run();
+            siv.add_layer(ListView::new()
+                          .with(|list| {
+                              stories.iter().enumerate().for_each(|(id, story)| {
+                                  list.add_child(
+                                      &format!("{}.", id),
+                                      TextView::new(story)
+                                  );
+                              });
+                          })
+                          .scrollable());
+            siv.add_global_callback('q', |s| s.quit());
+            siv.run();
+        }
+        Err(err) => {
+            eprintln!("{:?}", err);
+        }
     }
 }
