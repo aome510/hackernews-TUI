1828b0795d20409508dba24780513f06e9778fdf
Thang Pham <phamducthang1234@gmail.com>
2021-02-21 15:59:55 +0900

    wrap comment with linearlayout and textview

--- src/view.rs
+++ src/view.rs
@@ -1,9 +1,9 @@
 use super::hn_client::*;
 use cursive::{
     event::EventResult,
-    traits::Scrollable,
+    traits::*,
     view::IntoBoxedView,
-    views::{OnEventView, SelectView},
+    views::{self, OnEventView, SelectView},
 };
 use log::warn;
 
@@ -41,26 +41,44 @@ pub fn get_story_view(stories: Vec<Story>, hn_client: &HNClient) -> impl IntoBox
     .scrollable()
 }
 
+fn format_hn_text(s: String) -> String {
+    s.replace("<p>", "\n")
+}
+
 /// Return a cursive's View from a comment list
 fn get_comment_view(comments: Vec<Comment>, hn_client: &HNClient) -> impl IntoBoxedView {
-    let hn_client  = hn_client.clone();
-    construct_event_view(
-        SelectView::new().with_all(
-            comments
-                .into_iter()
-                .map(|comment| (format!("{}: {}", comment.by, comment.text), comment)),
-        ),
-    )
-    .on_event(cursive::event::Key::Backspace, move |s| {
-        match hn_client.get_top_stories() {
-            Ok(stories) => {
-                s.pop_layer();
-                s.add_layer(get_story_view(stories, &hn_client))
-            }
-            Err(err) => {
-                warn!("failed to get top stories: {:#?}", err);
-            }
-        }
-    })
-    .scrollable()
+    // let hn_client = hn_client.clone();
+    // construct_event_view(
+    //     SelectView::new().with_all(
+    //         comments
+    //             .into_iter()
+    //             .map(|comment| (format!("{}: {}", comment.by, comment.text), comment)),
+    //     ),
+    // )
+    // .on_event(cursive::event::Key::Backspace, move |s| {
+    //     match hn_client.get_top_stories() {
+    //         Ok(stories) => {
+    //             s.pop_layer();
+    //             s.add_layer(get_story_view(stories, &hn_client))
+    //         }
+    //         Err(err) => {
+    //             warn!("failed to get top stories: {:#?}", err);
+    //         }
+    //     }
+    // })
+    // .scrollable()
+    views::LinearLayout::vertical().with(|v| {
+        comments.into_iter().for_each(|comment| {
+            v.add_child(cursive::views::PaddedView::lrtb(
+                0,
+                0,
+                0,
+                1,
+                cursive::views::TextView::new(
+                    format_hn_text(
+                        format!("{}: {}", comment.by, comment.text))
+                        )
+            ));
+        })
+    }).scrollable()
 }
