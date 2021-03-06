610496183d8b6e718685ac8bece43a546f45117a
Thang Pham <phamducthang1234@gmail.com>
2021-03-06 23:25:10 +0900

    remove start from main

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,6 @@ pub mod view;
 use prelude::*;
 
 fn main() {
-    if let Err(err) = start() {
-        error!("{:#?}", err)
-    }
-}
-
-fn start() -> Result<()> {
     env_logger::init();
 
     let mut siv = cursive::default();
@@ -19,9 +13,8 @@ fn start() -> Result<()> {
     // load theme
     siv.load_toml(include_str!("../theme.toml")).unwrap();
 
-    let client = hn_client::HNClient::new()?;
+    let client = hn_client::HNClient::new().unwrap();
     let async_view = async_view::get_story_view_async(&mut siv, &client);
     siv.add_layer(async_view);
     siv.run();
-    Ok(())
 }
