689481d86d5a194c47b3caf24819bd86764d531c
Thang Pham <phamducthang1234@gmail.com>
2021-04-05 14:58:53 +0900

    implement page-like scrolling

--- src/view/list_view.rs
+++ src/view/list_view.rs
@@ -28,19 +28,19 @@ pub fn construct_scroll_list_event_view<T: ScrollableList>(view: T) -> OnEventVi
             }
         })
         .on_pre_event_inner(Key::Up, |s, _| {
-            s.get_scoller_mut().scroll_up(1);
+            s.get_scroller_mut().scroll_up(1);
             Some(EventResult::Consumed(None))
         })
         .on_pre_event_inner(Key::Down, |s, _| {
-            s.get_scoller_mut().scroll_down(1);
+            s.get_scroller_mut().scroll_down(1);
             Some(EventResult::Consumed(None))
         })
         .on_pre_event_inner(Key::PageUp, |s, _| {
-            s.get_scoller_mut().scroll_up(5);
+            s.get_scroller_mut().scroll_up(5);
             Some(EventResult::Consumed(None))
         })
         .on_pre_event_inner(Key::PageDown, |s, _| {
-            s.get_scoller_mut().scroll_down(5);
+            s.get_scroller_mut().scroll_down(5);
             Some(EventResult::Consumed(None))
         })
 }
@@ -51,7 +51,9 @@ pub trait ScrollableList {
     fn len(&self) -> usize;
     fn get_focus_index(&self) -> usize;
     fn set_focus_index(&mut self, id: usize) -> Option<EventResult>;
-    fn get_scoller_mut(&mut self) -> &mut scroll::Core;
+    fn get_scroller(&self) -> &scroll::Core;
+    fn get_scroller_mut(&mut self) -> &mut scroll::Core;
+    fn scroll(&mut self, direction: bool);
 }
 
 #[macro_export]
@@ -65,20 +67,63 @@ macro_rules! impl_scrollable_list {
             self.get_inner().get_inner().get_focus_index()
         }
 
+        fn scroll(&mut self, direction: bool) {
+            // (direction = true) stands for going down while false stands for going up
+            let important_area = self
+                .get_inner()
+                .get_inner()
+                .important_area(self.get_scroller().last_outer_size());
+            let scroller = self.get_scroller_mut();
+
+            // the below implementation is based on scroll_to_rect function
+            // defined in Cursive::view::scroll::core.rs
+            let top_left = (important_area.bottom_right() + (1, 1))
+                .saturating_sub(scroller.last_available_size());
+            let bottom_right = important_area.top_left();
+            let offset = scroller.content_viewport().top_left();
+
+            debug!(
+                "top_left: {:#?},\n bottom_right: {:#?},\n offset: {:#?}",
+                top_left, bottom_right, offset
+            );
+            // focused view has larger size than the what is provided to the scroller
+            if (bottom_right.y < top_left.y) {
+                scroller.set_offset(bottom_right);
+            } else {
+                if (direction) {
+                    // focused is moved down
+                    if (offset.y < top_left.y) {
+                        scroller.set_offset(bottom_right);
+                    }
+                } else {
+                    // focus is moved up
+                    if (offset.y > bottom_right.y) {
+                        scroller.set_offset(top_left);
+                    }
+                }
+            }
+        }
+
         fn set_focus_index(&mut self, id: usize) -> Option<EventResult> {
-            let scroll_view = self.get_inner_mut();
-            let linear_layout = scroll_view.get_inner_mut();
+            let current_id = self.get_focus_index();
+            let direction = if (current_id <= id) { true } else { false };
+            let linear_layout = self.get_inner_mut().get_inner_mut();
+
             match linear_layout.set_focus_index(id) {
                 Ok(_) => {
-                    scroll_view.scroll_to_important_area();
+                    self.scroll(direction);
                     Some(EventResult::Consumed(None))
                 }
                 Err(_) => None,
             }
         }
 
-        fn get_scoller_mut(&mut self) -> &mut scroll::Core {
+        fn get_scroller_mut(&mut self) -> &mut scroll::Core {
             self.get_inner_mut().get_scroller_mut()
+        }
+
+        fn get_scroller(&self) -> &scroll::Core {
+            self.get_inner().get_scroller()
         }
     };
 }
