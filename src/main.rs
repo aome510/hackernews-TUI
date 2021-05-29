a48dec9b750f433562b4c829fa385c6735153f95
Thang Pham <phamducthang1234@gmail.com>
2021-05-29 22:48:19 +0900

    release v0.6.2

--- src/main.rs
+++ src/main.rs
@@ -205,7 +205,7 @@ fn main() {
 
     // parse command line arguments
     let matches = App::new("hackernews-tui")
-        .version("0.6.1")
+        .version("0.6.2")
         .author("Thang Pham <phamducthang1234@gmail>")
         .arg(
             Arg::with_name("config")
