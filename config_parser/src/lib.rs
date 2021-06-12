4f39dc666f5734b6691b8097424d5f0516fec364
Thang Pham <phamducthang1234@gmail.com>
2021-06-12 11:55:56 +0900

    add more primitive types in config_parser

--- config_parser/src/lib.rs
+++ config_parser/src/lib.rs
@@ -30,4 +30,6 @@ impl<T: ConfigParser + Default> ConfigParser for Vec<T> {
     }
 }
 
-config_parser_impl!(String, usize, u64, u32, bool);
+config_parser_impl!(
+    String, usize, u128, u64, u32, u16, u8, isize, i128, i64, i32, i16, i8, f64, f32, bool, char
+);
