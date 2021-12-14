343c0e37fe2a5814cbc4347ae16706c9c43f04c8
Thang Pham <phamducthang1234@gmail.com>
2021-12-14 16:50:51 -0500

    delete all theme-related structs and construct new ones

--- hackernews_tui/src/config/theme.rs
+++ hackernews_tui/src/config/theme.rs
@@ -1,103 +1,52 @@
-use config_parser2::*;
 use serde::{de, Deserialize, Deserializer};
 
-#[derive(Debug, Clone)]
-pub struct Color {
-    pub color: cursive::theme::Color,
+#[derive(Clone, Debug, Deserialize)]
+pub struct Theme {
+    pub palette: Palette,
+    pub component_style: ComponentStyle,
 }
 
-impl Color {
-    fn parse(s: &str) -> Option<Self> {
-        cursive::theme::Color::parse(s).map(|color| Color { color })
-    }
+#[derive(Clone, Debug, Deserialize)]
+pub struct Palette {
+    pub background: Color,
+    pub foreground: Color,
+    pub selection_background: Color,
+    pub selection_foreground: Color,
+
+    pub black: Color,
+    pub blue: Color,
+    pub cyan: Color,
+    pub green: Color,
+    pub magenta: Color,
+    pub red: Color,
+    pub white: Color,
+    pub yellow: Color,
+
+    pub bright_black: Color,
+    pub bright_white: Color,
+    pub bright_red: Color,
+    pub bright_magenta: Color,
+    pub bright_green: Color,
+    pub bright_cyan: Color,
+    pub bright_blue: Color,
+    pub bright_yellow: Color,
 }
 
-impl<'de> de::Deserialize<'de> for Color {
+#[derive(Clone, Debug, Deserialize)]
+pub struct ComponentStyle {}
+
+#[derive(Clone, Debug)]
+pub struct Color(cursive::theme::Color);
+
+impl<'de> Deserialize<'de> for Color {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
-        match Color::parse(&s) {
+        match cursive::theme::Color::parse(&s) {
             None => Err(de::Error::custom(format!("failed to parse color: {}", s))),
-            Some(color) => Ok(color),
+            Some(color) => Ok(Color(color)),
         }
     }
-}
-
-config_parser_impl!(Color);
-
-#[derive(Debug, Deserialize, Clone, ConfigParse)]
-pub struct Theme {
-    // cursive's palette colors
-    pub background: Color,
-    pub view: Color,
-    pub shadow: Color,
-    pub primary: Color,
-    pub secondary: Color,
-    pub tertiary: Color,
-    pub title_primary: Color,
-    pub title_secondary: Color,
-    pub highlight: Color,
-    pub highlight_inactive: Color,
-    pub highlight_text: Color,
-
-    // additional custom colors
-    pub link_text: Color,
-    pub link_id_bg: Color,
-    pub search_highlight_bg: Color,
-    pub status_bar_bg: Color,
-    pub code_block_bg: Color,
-}
-
-impl Theme {
-    pub fn update_theme(&self, theme: &mut cursive::theme::Theme) {
-        theme.palette.set_color("background", self.background.color);
-        theme.palette.set_color("view", self.view.color);
-        theme.palette.set_color("shadow", self.shadow.color);
-        theme.palette.set_color("primary", self.primary.color);
-        theme.palette.set_color("secondary", self.secondary.color);
-        theme.palette.set_color("tertiary", self.tertiary.color);
-        theme
-            .palette
-            .set_color("title_primary", self.title_primary.color);
-        theme
-            .palette
-            .set_color("title_secondary", self.title_secondary.color);
-        theme.palette.set_color("highlight", self.highlight.color);
-        theme
-            .palette
-            .set_color("highlight_inactive", self.highlight_inactive.color);
-        theme
-            .palette
-            .set_color("highlight_text", self.highlight_text.color);
-    }
-}
-
-impl Default for Theme {
-    fn default() -> Self {
-        Theme {
-            background: Color::parse("#f6f6ef").unwrap(),
-            shadow: Color::parse("#000000").unwrap(),
-            view: Color::parse("#f6f6ef").unwrap(),
-            primary: Color::parse("#4a4a48").unwrap(),
-            secondary: Color::parse("#a5a5a5").unwrap(),
-            tertiary: Color::parse("#ffffff").unwrap(),
-            title_primary: Color::parse("#000000").unwrap(),
-            title_secondary: Color::parse("#ffff00").unwrap(),
-            highlight: Color::parse("#6c6c6c").unwrap(),
-            highlight_inactive: Color::parse("#0000ff").unwrap(),
-            highlight_text: Color::parse("#c3bbbb").unwrap(),
-
-            link_text: Color::parse("#4fbbfd").unwrap(),
-            link_id_bg: Color::parse("#ffff00").unwrap(),
-            search_highlight_bg: Color::parse("#ffff00").unwrap(),
-            status_bar_bg: Color::parse("#ff6600").unwrap(),
-            code_block_bg: Color::parse("#c8c8c8").unwrap(),
-        }
-    }
-}
-
-pub fn get_config_theme() -> &'static Theme {
-    &super::get_config().theme
 }
