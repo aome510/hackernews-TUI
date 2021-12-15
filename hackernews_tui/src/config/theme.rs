863fa4cdc15ded5dfb4bf0574aa0be054580e08e
Thang Pham <phamducthang1234@gmail.com>
2021-12-15 00:38:20 -0500

    add foreground color for `link_id` and `matched_highlight` component styles

--- hackernews_tui/src/config/theme.rs
+++ hackernews_tui/src/config/theme.rs
@@ -89,8 +89,8 @@ impl Default for ComponentStyle {
         Self {
             title_bar: ColorStyle::new(Color::parse("black"), Color::parse("#ff6600")),
             link: ColorStyle::front(Color::parse("#4fbbfd")),
-            link_id: ColorStyle::back(Color::parse("#ffff55")),
-            matched_highlight: ColorStyle::back(Color::parse("#ffff55")),
+            link_id: ColorStyle::new(Color::parse("black"), Color::parse("#ffff55")),
+            matched_highlight: ColorStyle::new(Color::parse("black"), Color::parse("#ffff55")),
             code_block: ColorStyle::back(Color::parse("#c8c8c8")),
             metadata: ColorStyle::front(Color::parse("#a5a5a5")),
         }
