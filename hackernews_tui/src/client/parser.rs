94762bad8acb40dacbf9c6c0d3b71aafe67d0598
Thang Pham <phamducthang1234@gmail.com>
2023-01-31 23:40:58 -0500

    remove text parsing codes from conversion functions in `parser.rs`

--- hackernews_tui/src/client/parser.rs
+++ hackernews_tui/src/client/parser.rs
@@ -143,131 +143,35 @@ impl From<StoryResponse> for Story {
             .unwrap_or_default();
         let title = decode_html(&title);
 
-        let mut parsed_title = StyledString::new();
-
-        // parse the story title and decorate it based on the story category
-        let title = {
-            if let Some(title) = title.strip_prefix("Ask HN") {
-                parsed_title
-                    .append_styled("Ask HN", config::get_config_theme().component_style.ask_hn);
-                title
-            } else if let Some(title) = title.strip_prefix("Tell HN") {
-                parsed_title.append_styled(
-                    "Tell HN",
-                    config::get_config_theme().component_style.tell_hn,
-                );
-                title
-            } else if let Some(title) = title.strip_prefix("Show HN") {
-                parsed_title.append_styled(
-                    "Show HN",
-                    config::get_config_theme().component_style.show_hn,
-                );
-                title
-            } else if let Some(title) = title.strip_prefix("Launch HN") {
-                parsed_title.append_styled(
-                    "Launch HN",
-                    config::get_config_theme().component_style.launch_hn,
-                );
-                title
-            } else {
-                &title
-            }
-        };
-
-        // parse the story title that may contain search matches wrapped inside `<em>` tags
-        // The matches are decorated with a corresponding style.
-        {
-            // an index such that `title[curr_pos..]` represents the part of the
-            // text that hasn't been parsed.
-            let mut curr_pos = 0;
-            for caps in MATCH_RE.captures_iter(title) {
-                let whole_match = caps.get(0).unwrap();
-                // the part that doesn't match any patterns should be rendered in the default style
-                if curr_pos < whole_match.start() {
-                    parsed_title.append_plain(&title[curr_pos..whole_match.start()]);
-                }
-                curr_pos = whole_match.end();
-
-                parsed_title.append_styled(
-                    caps.name("match").unwrap().as_str(),
-                    config::get_config_theme().component_style.matched_highlight,
-                );
-            }
-            if curr_pos < title.len() {
-                parsed_title.append_plain(&title[curr_pos..title.len()]);
-            }
-        }
-
-        let author = s.author.unwrap_or_else(|| String::from("[deleted]"));
-
         // parse story's text
-        let text = {
-            let metadata = utils::combine_styled_strings(vec![
-                StyledString::plain(parsed_title.source()),
-                StyledString::plain("\n"),
-                StyledString::styled(
-                    format!(
-                        "{} points | by {} | {} ago | {} comments\n",
-                        s.points,
-                        author,
-                        utils::get_elapsed_time_as_text(s.time),
-                        s.num_comments,
-                    ),
-                    config::get_config_theme().component_style.metadata,
-                ),
-            ]);
-
-            // the HTML story text returned by HN Algolia API doesn't wrap a
-            // paragraph inside a `<p><\p>` tag pair.
-            // Instead, it seems to use `<p>` to represent a paragraph break.
-            let mut story_text = decode_html(&s.text.unwrap_or_default()).replace("<p>", "\n\n");
-
-            let minimized_text = if story_text.is_empty() {
-                metadata.clone()
-            } else {
-                story_text = format!("\n{story_text}");
-
-                utils::combine_styled_strings(vec![
-                    metadata.clone(),
-                    StyledString::plain("... (more)"),
-                ])
-            };
-
-            let mut text = metadata;
-            let result = parse_hn_html_text(story_text, Style::default(), 0);
-            text.append(result.s);
-
-            HnItem {
-                id: s.id,
-                level: 0,
-                state: DisplayState::Normal,
-                minimized_text,
-                text,
-                links: result.links,
-            }
+        let content = {
+            // The HTML story text returned by HN Algolia API doesn't wrap a paragraph inside a `<p><\p>` tag pair.
+            // Instead, it seems to use `<p>` to represent a paragraph break, so we need to do search/replace
+            // to parse the text into a styled string.
+            decode_html(&s.text.unwrap_or_default()).replace("<p>", "\n\n")
         };
 
         Story {
-            title: parsed_title,
             url: s.url.unwrap_or_default(),
-            author,
+            author: s.author.unwrap_or_default(),
             id: s.id,
             points: s.points,
             num_comments: s.num_comments,
             time: s.time,
-            text,
+            title,
+            content,
         }
     }
 }
 
-impl From<CommentResponse> for Vec<HnItem> {
+impl From<CommentResponse> for Vec<Comment> {
     fn from(c: CommentResponse) -> Self {
         // recursively parse child comments of the current comment
         let children = c
             .children
             .into_par_iter()
             .filter(|comment| comment.author.is_some() && comment.text.is_some())
-            .flat_map(<Vec<HnItem>>::from)
+            .flat_map(<Vec<Comment>>::from)
             .map(|mut c| {
                 c.level += 1; // update the level of every children comments
                 c
@@ -276,40 +180,12 @@ impl From<CommentResponse> for Vec<HnItem> {
 
         // parse current comment
         let comment = {
-            let metadata = utils::combine_styled_strings(vec![
-                StyledString::styled(
-                    c.author.unwrap_or_default(),
-                    config::get_config_theme().component_style.username,
-                ),
-                StyledString::styled(
-                    format!(" {} ago ", utils::get_elapsed_time_as_text(c.time)),
-                    config::get_config_theme().component_style.metadata,
-                ),
-            ]);
-
-            let mut text =
-                utils::combine_styled_strings(vec![metadata.clone(), StyledString::plain("\n")]);
-
-            let result = parse_hn_html_text(
-                decode_html(&c.text.unwrap_or_default()),
-                Style::default(),
-                0,
-            );
-            text.append(result.s);
-
-            HnItem {
+            Comment {
                 id: c.id,
                 level: 0,
-                state: DisplayState::Normal,
-                minimized_text: utils::combine_styled_strings(vec![
-                    metadata,
-                    StyledString::styled(
-                        format!("({} more)", children.len() + 1),
-                        config::get_config_theme().component_style.metadata,
-                    ),
-                ]),
-                text,
-                links: result.links,
+                time: c.time,
+                author: c.author.unwrap_or_default(),
+                content: c.text.unwrap_or_default(),
             }
         };
 
--- hackernews_tui/src/model.rs
+++ hackernews_tui/src/model.rs
@@ -15,8 +15,8 @@ pub struct Story {
     pub points: u32,
     pub num_comments: usize,
     pub time: u64,
-    pub title: StyledString, // story's title can be represented as a styled string in SearchView
-    pub content: StyledString,
+    pub title: String,
+    pub content: String,
 }
 
 #[derive(Debug, Clone)]
@@ -25,7 +25,7 @@ pub struct Comment {
     pub level: usize,
     pub author: String,
     pub time: u64,
-    pub content: StyledString,
+    pub content: String,
 }
 
 pub struct StoryHiddenData {
@@ -80,3 +80,128 @@ impl Story {
         format!("{}/item?id={}", client::HN_HOST_URL, self.id)
     }
 }
+
+//// parse story's text
+// let text = {
+//     let metadata = utils::combine_styled_strings(vec![
+//         StyledString::plain(parsed_title.source()),
+//         StyledString::plain("\n"),
+//         StyledString::styled(
+//             format!(
+//                 "{} points | by {} | {} ago | {} comments\n",
+//                 s.points,
+//                 author,
+//                 utils::get_elapsed_time_as_text(s.time),
+//                 s.num_comments,
+//             ),
+//             config::get_config_theme().component_style.metadata,
+//         ),
+//     ]);
+
+//     // the HTML story text returned by HN Algolia API doesn't wrap a
+//     // paragraph inside a `<p><\p>` tag pair.
+//     // Instead, it seems to use `<p>` to represent a paragraph break.
+//     let mut story_text = decode_html(&s.text.unwrap_or_default()).replace("<p>", "\n\n");
+
+//     let minimized_text = if story_text.is_empty() {
+//         metadata.clone()
+//     } else {
+//         story_text = format!("\n{story_text}");
+
+//         utils::combine_styled_strings(vec![
+//             metadata.clone(),
+//             StyledString::plain("... (more)"),
+//         ])
+//     };
+
+//     let mut text = metadata;
+//     let result = parse_hn_html_text(story_text, Style::default(), 0);
+//     text.append(result.s);
+
+//     HnItem {
+//         id: s.id,
+//         level: 0,
+//         state: DisplayState::Normal,
+//         minimized_text,
+//         text,
+//         links: result.links,
+//     }
+// };
+
+//// parse story title
+// let mut parsed_title = StyledString::new();
+
+// // parse the story title and decorate it based on the story category
+// let title = {
+//     if let Some(title) = title.strip_prefix("Ask HN") {
+//         parsed_title
+//             .append_styled("Ask HN", config::get_config_theme().component_style.ask_hn);
+//         title
+//     } else if let Some(title) = title.strip_prefix("Tell HN") {
+//         parsed_title.append_styled(
+//             "Tell HN",
+//             config::get_config_theme().component_style.tell_hn,
+//         );
+//         title
+//     } else if let Some(title) = title.strip_prefix("Show HN") {
+//         parsed_title.append_styled(
+//             "Show HN",
+//             config::get_config_theme().component_style.show_hn,
+//         );
+//         title
+//     } else if let Some(title) = title.strip_prefix("Launch HN") {
+//         parsed_title.append_styled(
+//             "Launch HN",
+//             config::get_config_theme().component_style.launch_hn,
+//         );
+//         title
+//     } else {
+//         &title
+//     }
+// };
+
+// // parse the story title that may contain search matches wrapped inside `<em>` tags
+// // The matches are decorated with a corresponding style.
+// {
+//     // an index such that `title[curr_pos..]` represents the part of the
+//     // text that hasn't been parsed.
+//     let mut curr_pos = 0;
+//     for caps in MATCH_RE.captures_iter(title) {
+//         let whole_match = caps.get(0).unwrap();
+//         // the part that doesn't match any patterns should be rendered in the default style
+//         if curr_pos < whole_match.start() {
+//             parsed_title.append_plain(&title[curr_pos..whole_match.start()]);
+//         }
+//         curr_pos = whole_match.end();
+
+//         parsed_title.append_styled(
+//             caps.name("match").unwrap().as_str(),
+//             config::get_config_theme().component_style.matched_highlight,
+//         );
+//     }
+//     if curr_pos < title.len() {
+//         parsed_title.append_plain(&title[curr_pos..title.len()]);
+//     }
+// }
+
+// // parse comment
+// let metadata = utils::combine_styled_strings(vec![
+//     StyledString::styled(
+//         c.author.unwrap_or_default(),
+//         config::get_config_theme().component_style.username,
+//     ),
+//     StyledString::styled(
+//         format!(" {} ago ", utils::get_elapsed_time_as_text(c.time)),
+//         config::get_config_theme().component_style.metadata,
+//     ),
+// ]);
+
+// let mut text =
+//     utils::combine_styled_strings(vec![metadata.clone(), StyledString::plain("\n")]);
+
+// let result = parse_hn_html_text(
+//     decode_html(&c.text.unwrap_or_default()),
+//     Style::default(),
+//     0,
+// );
+// text.append(result.s);
